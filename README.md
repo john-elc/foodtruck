4031bd153b047726c29869926fc0be17dfba7913
John <jmullany@outlook.com>
2023-04-17 12:55:29 -0500

    initialize project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Food Truck Frontend, Backend, API and Postgres DB
+To start the project:
+  
+  * You need to have docker installed. Directions are at the following url `https://docs.docker.com/engine/install/`
+  * Run `docker compose up` to create a docker container with the necessary images
+  * It should automatically start the browser on port 8000 but you may have to open a browser and go to `http://localhost:8000`
+  
+When finished:
+  * Run `docker compose down`
+
+To run Elixir API Test:
+  * cd into the food_truck_api directory
+  * Run `MIX_ENV=test mix test` which currently runs 16 tests
+  
+I wish I had more time for styling and tests and a bit more functionality. I would be happy to finish it out if this does not meet the requirements
+
+Thanks,
+John
---
+++ food_truck_frontend/src/components/FoodTruckSearch.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { AiOutlineSearch } from "react-icons/ai";
+import FoodTrucks from "./FoodTrucks";
+
+function FoodTruckSearch() {
+  // const [foodTrucks, setFoodTrucks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchFoodTrucks = async (cuisine) => {
+    const response = await axios.get(
+      `http://localhost:4000/api/trucks?food_items=${cuisine}`
+    );
+
+    setFoodTrucks(response.data.trucks);
+  };
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchFoodTrucks(searchTerm);
+    }
+  }, [searchTerm]);
+
+  return (
+    <div className="searchDiv grid gap-10 bg-gray-300 rounded-[10px] p-[1rem]">
+      <div className="firstDiv flex  justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-grayIsh-700">
+        <div className="flex gap-2 items-center">
+          <AiOutlineSearch className="text-[25px] icon" />
+          <input
+            type="text"
+            className="bg-transparent text-blue-500 focus:outline-none w-full"
+            placeholder="Search by Cuisine Here...."
+            onChange={(event) => setSearchTerm(event.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FoodTruckSearch;
